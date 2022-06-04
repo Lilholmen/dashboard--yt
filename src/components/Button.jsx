69d4e8e31a517c27e7cf6e4d09a27bb7771d0f3d
lilholmen <lilholmen@yandex.ru>
2022-06-04 20:22:07 +0300

    syncfusion not response for sparkline chart

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
 
-const Button = () => {
-  return <div>Button</div>;
+const Button = ({ bgColor, color, borderRadius, text, size }) => {
+  return (
+    <button
+      type="button"
+      style={{
+        backgroundColor: bgColor,
+        color,
+        borderRadius,
+      }}
+      className={`text-${size} p-3 hover:drop-shadow-xl`}
+    >
+      {text}
+    </button>
+  );
 };
 
 export default Button;
