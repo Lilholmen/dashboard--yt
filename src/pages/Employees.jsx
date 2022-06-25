df120523dadf078432c6373d8c3963f2bbda427e
lilholmen <lilholmen@yandex.ru>
2022-06-25 17:31:49 +0300

    complete oreders, employees and customers pages

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,7 +1,36 @@
 import React from 'react';
+import {
+  GridComponent,
+  ColumnDirective,
+  ColumnsDirective,
+  Search,
+  Page,
+  Inject,
+  Toolbar,
+} from '@syncfusion/ej2-react-grids';
+
+import { employeesData, employeesGrid } from '../data/dummy';
+import { Header } from '../components';
 
 const Employees = () => {
-  return <div>Employees</div>;
+  return (
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="Page" title="Employees" />
+      <GridComponent
+        dataSource={employeesData}
+        allowPaging
+        toolbar={['Search']}
+        width="auto"
+      >
+        <ColumnsDirective>
+          {employeesGrid.map((item, index) => (
+            <ColumnDirective key={index} {...item} />
+          ))}
+        </ColumnsDirective>
+        <Inject services={[Search, Page, Toolbar]} />
+      </GridComponent>
+    </div>
+  );
 };
 
 export default Employees;
